9195cb2721159b7a8d502a0e86ded02573a5d51b
Ernest Franssen <efran7107@gmail.com>
2023-07-26 17:36:03 -0600

    commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+let turns = 2;
+let mark = (turns % 2 == 0) ? 'X' : 'O';
+
+
 const buttonOne = () => {
     document.getElementById('b1').value = 'X';
     document.getElementById('b1').style.backgroundColor = 'red'
+    turn++;
 };
+
+const buttonTwo = () => {
+    
+}
+
